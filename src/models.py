d3041328b8d45c5d25aa363d700de032f10d095a
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-06-07 21:32:32 +0300

    Remove useless 'ScheduledEvent' class

--- src/models.py
+++ src/models.py
@@ -10,28 +10,6 @@ load_dotenv()
 database = connect(os.getenv("DATABASE_URL", default="sqlite:///db.sqlite3"))
 
 
-class ScheduledEvent:
-    def __init__(self, author, subject, expiration_date, notification_date=None):
-        # FK (or ID at least) of user who created event
-        self.author = author
-
-        # what user has done and subject of notification in the future ("started use new toothbrush")
-        self.subject = subject
-
-        # DateField, when user should be notified second (last) time (action should be already taken)
-        self.expiration_date = expiration_date
-
-        # DateField, when user wants to be notified first time - beforehand
-        # might be calculated automatically - about 2/3 of expiration date, 2 weeks before or so
-        self.notification_date = notification_date
-
-        # DateField, should be something like auto_now_add (automatically set, no need to pass)
-        self.created = None
-
-        # flag whether the event is done or not yet. Set when notification goes off at self.notification_date
-        self.completed = False
-
-
 class User(pw.Model):
     user_id = pw.IntegerField(unique=True)
     chat_id = pw.IntegerField()
@@ -45,10 +23,19 @@ class User(pw.Model):
 
 class Event(pw.Model):
     author = pw.ForeignKeyField(User, backref="events")
+
+    # what user has done and subject of notification in the future ("started use new toothbrush")
     subject = pw.CharField()
+
+    # when user should be notified second (last) time (action should be already taken)
     expiration_date = pw.DateField()
+
+    # when user wants to be notified first time - beforehand
     notification_date = pw.DateField()
+
     created = pw.DateField(default=datetime.datetime.now)
+
+    # flag whether the event is done or not yet. Set when notification goes off at self.notification_date
     completed = pw.BooleanField(default=False)
 
     class Meta:
