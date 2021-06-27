84ff4c7750b8aa24e4f9db203145e788e0504913
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-06-27 17:38:35 +0300

    Move scheduled tasks to separate 'jobs.py'

--- src/bot.py
+++ src/bot.py
@@ -3,43 +3,13 @@ from datetime import time
 
 import pytz
 from dotenv import load_dotenv
-from telegram.ext import CallbackContext, Updater
+from telegram.ext import Updater
 
 from handlers.main import setup_dispatcher
-from models import User, complete_event, create_tables, get_events_for_expiration, get_events_for_notification
-from utils import prettify_date
+from jobs import check_events_for_expiration, check_events_for_notification
+from models import create_tables
 
 load_dotenv()
-
-
-def check_events_for_notification(context: CallbackContext):
-    for user in User.select():
-        today_events_to_notify = get_events_for_notification(user.id)
-
-        for event in today_events_to_notify:
-            context.bot.send_message(
-                chat_id=user.chat_id,
-                text=f"You wanted me to notify you about:"
-                f"Subject: {event.subject}\n"
-                f"Notification date: {prettify_date(event.notification_date)}\n"
-                f"Expiration date: {prettify_date(event.expiration_date)}\n",
-            )
-
-
-def check_events_for_expiration(context: CallbackContext):
-    for user in User.select():
-        today_events_to_expire = get_events_for_expiration(user.id)
-
-        for event in today_events_to_expire:
-            context.bot.send_message(
-                chat_id=user.chat_id,
-                text=f"This event is expired today:"
-                f"Subject: {event.subject}\n"
-                f"Expiration date: {prettify_date(event.expiration_date)}\n"
-                f"(you were notified on: {prettify_date(event.notification_date)})\n",
-            )
-
-            complete_event(event.id)
 
 
 def main():
---
+++ src/jobs.py
@@ -0,0 +1,38 @@
+from telegram.ext import CallbackContext
+
+from models import User, complete_event, get_events_for_expiration, get_events_for_notification
+from utils import prettify_date
+
+
+def check_events_for_notification(context: CallbackContext):
+    """Fetch user events that should be notified today and send messages to the user"""
+
+    for user in User.select():
+        today_events_to_notify = get_events_for_notification(user.id)
+
+        for event in today_events_to_notify:
+            context.bot.send_message(
+                chat_id=user.chat_id,
+                text=f"You wanted me to notify you about:"
+                f"Subject: {event.subject}\n"
+                f"Notification date: {prettify_date(event.notification_date)}\n"
+                f"Expiration date: {prettify_date(event.expiration_date)}\n",
+            )
+
+
+def check_events_for_expiration(context: CallbackContext):
+    """Fetch user events that should expired today and send messages to the user"""
+
+    for user in User.select():
+        today_events_to_expire = get_events_for_expiration(user.id)
+
+        for event in today_events_to_expire:
+            context.bot.send_message(
+                chat_id=user.chat_id,
+                text=f"This event is expired today:"
+                f"Subject: {event.subject}\n"
+                f"Expiration date: {prettify_date(event.expiration_date)}\n"
+                f"(you were notified on: {prettify_date(event.notification_date)})\n",
+            )
+
+            complete_event(event.id)
