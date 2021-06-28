1551a24d842a66e1ce85ad68c4ec5ef8c92dee40
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-06-28 19:24:42 +0300

    Move some handlers from 'other.py' to 'main.py'

--- src/handlers/main.py
+++ src/handlers/main.py
@@ -1,4 +1,6 @@
-from telegram.ext import CallbackQueryHandler, CommandHandler, ConversationHandler, Filters, MessageHandler
+from telegram import ReplyKeyboardMarkup, Update
+from telegram.ext import CallbackContext, ConversationHandler
+from telegram.ext import CallbackQueryHandler, CommandHandler, Filters, MessageHandler
 
 from handlers.conversation import (
     CONFIRMATION,
@@ -14,8 +16,52 @@ from handlers.conversation import (
     start_creating_entry,
 )
 from handlers.list import complete_event_handler, delete_event_handler, show_expired, show_pending
-from handlers.others import cancel, help_handler, start
+from models import User
 from utils import error_logger
+from utils import get_description, logger
+
+
+def help_handler(update: Update, _: CallbackContext) -> None:
+    update.message.reply_text(f"Supported commands:\n{get_description()}")
+
+
+def cancel(update: Update, context: CallbackContext) -> int:
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text("The data you've entered previously is gone. Start again!")
+
+    context.user_data.clear()
+
+    return ConversationHandler.END
+
+
+def start(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /start is issued."""
+
+    current_user = update.effective_user
+    chat_id = update.message.chat_id
+
+    update.message.reply_markdown_v2(fr"Hi {current_user.mention_markdown_v2()}\!")
+
+    if not User.select().where(User.user_id == current_user.id):
+        User.create(
+            user_id=current_user.id,
+            chat_id=chat_id,
+            first_name=current_user.first_name,
+            last_name=current_user.last_name,
+            username=current_user.username,
+        )
+
+    # Save "user_id" for future linking with new event
+    context.user_data["user_id"] = current_user.id
+
+    reply_keyboard = [
+        ["/add", "/list"],
+        ["/list_expired", "/help"],
+    ]
+
+    markup = ReplyKeyboardMarkup(reply_keyboard, resize_keyboard=True)
+    update.message.reply_text(f"The available commands for now:\n{get_description()}", reply_markup=markup)
 
 
 def setup_dispatcher(dispatcher):
