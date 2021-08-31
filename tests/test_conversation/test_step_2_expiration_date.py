319d36260669c71b978997bf3779ca0a24e9f2a6
DerevenetsArtyom <derevenetsartyom@gmail.com>
2021-08-31 10:22:01 +0300

    Improve new tests - comments, variables, etc.

--- tests/test_conversation/test_step_2_expiration_date.py
+++ tests/test_conversation/test_step_2_expiration_date.py
@@ -34,25 +34,24 @@ def test_add_expiration_date_custom_handler_invalid_date(bot_app, update, contex
     assert return_value == EXPIRATION_DATE
 
 
-@pytest.mark.parametrize("desired_time_period", ["week", "month", "3month"])
-def test_add_expiration_date_from_choice(bot_app, update, context, desired_time_period):
-    update.callback_query.data = f"expiration_date:{desired_time_period}"  # emulate user's correct choice
+@pytest.mark.parametrize("user_expiration_choice", ["week", "month", "3month"])
+def test_add_expiration_date_from_choice(bot_app, update, context, user_expiration_choice):
+    update.callback_query.data = f"expiration_date:{user_expiration_choice}"  # emulate user's correct choice
 
     return_value = bot_app.call("add_expiration_date_from_choice", update, context)
-
-    assert "You've added an expiration date" in update.callback_query.message.reply_text.call_args[0][0]
 
     expiration_date = None
     now = datetime.datetime.now().date()
 
-    if desired_time_period == "week":
+    if user_expiration_choice == "week":
         expiration_date = now + timedelta(weeks=1)
-    elif desired_time_period == "month":
+    elif user_expiration_choice == "month":
         expiration_date = now + timedelta(days=30)
-    elif desired_time_period == "3month":
+    elif user_expiration_choice == "3month":
         expiration_date = now + timedelta(days=92)
 
     assert context.user_data["expiration_date"] == expiration_date
+    assert "You've added an expiration date" in update.callback_query.message.reply_text.call_args[0][0]
 
     # Correct step is returned for correct conversation flow
     assert return_value == NOTIFICATION_DATE
@@ -62,6 +61,9 @@ def test_add_expiration_date_from_choice_invalid_choice(bot_app, update, context
     update.callback_query.data = "invalid-choice-user-somehow-put"
 
     return_value = bot_app.call("add_expiration_date_from_choice", update, context)
+
+    # Invalid 'expiration_date' was not stored in 'user_data'
+    assert "expiration_date" not in context.user_data
 
     # We print warning and abort the flow
     assert update.callback_query.message.reply_text.call_args[0][0] == "Something went wrong. Don't know your choice"
--- tests/test_conversation/test_step_3_notification_date.py
+++ tests/test_conversation/test_step_3_notification_date.py
@@ -19,7 +19,6 @@ def test_add_notification_date_custom_handler(bot_app, update, context):
 
     assert "You've added notification date" in update.message.reply_text.call_args[0][0]
 
-    # We store 'notification_date' in 'user_data' for the future reference
     assert str(context.user_data["notification_date"]) == "2021-12-01"  # DB keeps dates in slightly different format
 
     # Correct step is returned for correct conversation flow
@@ -34,7 +33,7 @@ def test_add_notification_date_custom_handler_invalid_date(bot_app, update, cont
 
     assert "The date format is wrong" in update.message.reply_text.call_args[0][0]
 
-    # Invalid 'expiration_date' was not stored in 'user_data'
+    # Invalid 'notification_date' was not stored in 'user_data'
     assert "notification_date" not in context.user_data
 
     # Correct step is returned for correct conversation flow
@@ -44,6 +43,7 @@ def test_add_notification_date_custom_handler_invalid_date(bot_app, update, cont
 @pytest.mark.parametrize("user_notification_choice", ["week", "month", "3month"])
 def test_add_notification_date_from_choice(bot_app, update, context, user_notification_choice):
     expiration_date = datetime.date(2021, 12, 12)
+    notification_date = None
 
     update.callback_query.data = f"notification_date:{user_notification_choice}"  # emulate user's correct choice
 
@@ -53,10 +53,6 @@ def test_add_notification_date_from_choice(bot_app, update, context, user_notifi
 
     return_value = bot_app.call("add_notification_date_from_choice", update, context)
 
-    assert "You've added notification date" in update.callback_query.message.reply_text.call_args[0][0]
-
-    notification_date = None
-
     if user_notification_choice == "week":
         notification_date = expiration_date - timedelta(weeks=1)
     elif user_notification_choice == "month":
@@ -65,6 +61,7 @@ def test_add_notification_date_from_choice(bot_app, update, context, user_notifi
         notification_date = expiration_date - timedelta(days=92)
 
     assert context.user_data["notification_date"] == notification_date
+    assert "You've added notification date" in update.callback_query.message.reply_text.call_args[0][0]
 
     # Correct step is returned for correct conversation flow
     assert return_value == CONFIRMATION
@@ -74,6 +71,9 @@ def test_add_notification_date_from_choice_invalid_choice(bot_app, update, conte
     update.callback_query.data = "invalid-choice-user-somehow-put"
 
     return_value = bot_app.call("add_notification_date_from_choice", update, context)
+
+    # Invalid 'notification_date' was not stored in 'user_data'
+    assert "notification_date" not in context.user_data
 
     # We print warning and abort the flow
     assert update.callback_query.message.reply_text.call_args[0][0] == "Something went wrong. Don't know your choice"
